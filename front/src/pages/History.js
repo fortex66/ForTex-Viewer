ef5f4bd229ee7d1a4f25b63c3dc0d5bd353abb13
Frozen <steve.son8150@gmail.com>
2024-02-02 22:54:40 +0900

    history 페이지 그래프 캡쳐, 데이터 다운로드 기능 추가 & 백엔드 모드버스 TCP 통신 코드 수정 및 에러 처리 기능 추가

--- front/src/pages/History.js
+++ front/src/pages/History.js
@@ -5,13 +5,21 @@ import Header from '../components/Header';
 import Sidebar from '../components/Sidebar';
 import moment from 'moment-timezone';
 import { Line } from 'react-chartjs-2';
+import { saveAs } from 'file-saver';
+import html2canvas from 'html2canvas';
 import 'chartjs-adapter-moment';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faFileCsv,
+    faImage
+} from "@fortawesome/free-solid-svg-icons";
 
 const History = () => {
     const [startDateTime, setStartDateTime] = useState('');
     const [endDateTime, setEndDateTime] = useState('');
     const [temperatureData, setTemperatureData] = useState([]);
     const [interval, setInterval] = useState('60'); // 데이터 포인트 간격 (초 단위, 기본값 1분)
+    const [verify, setVerify] = useState(false); // 데이터 조회 여부 검사
 
 
     // 데이터 간격 선택 핸들러
@@ -45,10 +53,43 @@ const History = () => {
             });
 
             setTemperatureData({ labels: chartLabels, data: chartData });
+
+            setVerify(true);
         } catch (error) {
             console.error('Error fetching temperature history:', error);
+            setVerify(false);
         }
     };
+
+    // 조회 시작 시간과 종료 시간을 기반으로 적절한 time unit과 stepSize를 결정하는 함수
+    function getTimeScaleOptions(startDateTime, endDateTime) {
+        const duration = moment(endDateTime).diff(moment(startDateTime), 'hours');
+        let unit = 'minute';
+        let stepSize = 1;
+        let displayFormat = 'YYYY-MM-DD HH:mm';
+
+        if (duration <= 24) { // 24시간 이하
+            unit = 'minute';
+            stepSize = 30; // 30분 간격
+        } else if (duration <= 72) { // 3일 이하
+            unit = 'hour';
+            stepSize = 1; // 1시간 간격
+        } else { // 3일 이상
+            unit = 'day';
+            stepSize = 1; // 1일 간격
+            displayFormat = 'YYYY-MM-DD';
+        }
+
+        return {
+            unit: unit,
+            stepSize: stepSize,
+            tooltipFormat: 'YYYY-MM-DD HH:mm:ss',
+            displayFormats: {
+                [unit]: displayFormat
+            },
+        };
+    }
+
 
 
     // 차트 데이터 설정
@@ -67,29 +108,23 @@ const History = () => {
         scales: {
             x: {
                 type: 'time',
-                time: {
-                    unit: 'second',
-                    tooltipFormat: 'YYYY-MM-DD HH:mm:ss', // 툴팁에 표시될 시간 형식
-                    displayFormats: {
-                        second: 'YYYY-MM-DD HH:mm:ss', // x축 레이블에 표시될 시간 형식
-                    },
-                },
+                time: getTimeScaleOptions(startDateTime, endDateTime),
                 title: {
                     display: true,
                     text: 'Time',
                 },
             },
             y: {
-                beginAtZero: true, // y축의 시작점을 0으로 설정
+                beginAtZero: true,
             },
         },
     };
     
 
-
+    // 조회 시작 시간, 끝 시간에 따른 처리
     const handleTimeChange = (value, type) => {
         if (value) {
-            let timeString = value + ':00';
+            let timeString = value + ':00'; // 선택된 시간에 초를 00초로 추가
             let currentDateTime = type === 'start' ? startDateTime : endDateTime;
             currentDateTime = currentDateTime ? moment(currentDateTime) : moment();
     
@@ -101,6 +136,7 @@ const History = () => {
         }
     };
     
+    // 조회 시작 날짜, 끝 날짜에 따른 처리
     const handleDateChange = (value, type) => {
         if (value) {
             let date = moment(value);
@@ -114,6 +150,62 @@ const History = () => {
         }
     };
 
+    // 데이터를 CSV 형식으로 변환하는 함수
+    const convertToCSV = (data) => {
+        // CSV 파일의 헤더
+        let csvContent = "날짜,시간,온도\n";
+
+        // 각 데이터 포인트를 새 줄에 추가
+        data.forEach(item => {
+            const date = moment(item.timestamp).format('YYYY-MM-DD');
+            const time = moment(item.timestamp).format('HH:mm:ss');
+            csvContent += `${date},${time},${item.temperature}\n`;
+        });
+
+        return csvContent;
+    };
+
+    // 데이터를 CSV 파일로 저장하는 함수
+    const handleSaveDataAsCSV = () => {
+        if (!verify) {
+            alert("먼저 데이터를 조회해주세요.");
+            return;
+        }
+        saveDataAsCSV(temperatureData.data.map((temp, index) => ({
+            timestamp: temperatureData.labels[index],
+            temperature: temp
+        })));
+    };
+
+    
+    // CSV 파일로 저장하는 함수
+    const saveDataAsCSV = (data) => {
+        const BOM = '\uFEFF'; // 한글 입력시 깨짐 방지
+
+        const csvContent = convertToCSV(data);
+        const blob = new Blob([BOM + csvContent], { type: "text/csv;charset=utf-8;" });
+        saveAs(blob, "temperature-data.csv");
+    };
+
+    // 그래프를 이미지로 저장하는 함수
+    const handleCaptureGraph = () => {
+        if (!verify) {
+            alert("먼저 데이터를 조회해주세요.");
+            return;
+        }
+        captureGraph();
+    }
+
+    // 그래프 캡쳐
+    const captureGraph = () => {
+        const graphElement = document.getElementById('graph-container'); // 그래프가 포함된 컨테이너의 ID
+        html2canvas(graphElement).then(canvas => {
+          canvas.toBlob(function(blob) {
+            saveAs(blob, "graph-capture.png");
+          });
+        });
+      };
+
     return (
         <div>
             <Header></Header>
@@ -121,36 +213,43 @@ const History = () => {
                 <Sidebar></Sidebar>
                 <Contents>
                     <Title>온도 로그</Title>
-                    <SearchArea>
-                        <DateInput
-                            type="date"
-                            value={startDateTime ? startDateTime.split('T')[0] : ""}
-                            onChange={(e) => handleDateChange(e.target.value, 'start')}
-                        />
-                        <TimeInput
-                            type="time"
-                            value={startDateTime ? startDateTime.split('T')[1].substring(0, 5) : ""}
-                            onChange={(e) => handleTimeChange(e.target.value, 'start')}
-                        />
-                        <DateInput
-                            type="date"
-                            value={endDateTime ? endDateTime.split('T')[0] : ""}
-                            onChange={(e) => handleDateChange(e.target.value, 'end')}
-                        />
-                        <TimeInput
-                            type="time"
-                            value={endDateTime ? endDateTime.split('T')[1].substring(0, 5): ""}
-                            onChange={(e) => handleTimeChange(e.target.value, 'end')}
-                        />
-                        <DurationSelect value={interval} onChange={handleIntervalChange}>
-                            <option value="60">1분</option>
-                            <option value="600">10분</option>
-                            <option value="1800">30분</option>
-                            <option value="3600">1시간</option>
-                        </DurationSelect>
-                        <SearchButton onClick={handleSearch}>조회</SearchButton>
-                    </SearchArea>
-                    <ChartArea>
+                        <Top>
+                        <SearchArea>
+                            <DateInput
+                                type="date"
+                                value={startDateTime ? startDateTime.split('T')[0] : ""}
+                                onChange={(e) => handleDateChange(e.target.value, 'start')}
+                            />
+                            <TimeInput
+                                type="time"
+                                value={startDateTime ? startDateTime.split('T')[1].substring(0, 5) : ""}
+                                onChange={(e) => handleTimeChange(e.target.value, 'start')}
+                            />
+                            <DateInput
+                                type="date"
+                                value={endDateTime ? endDateTime.split('T')[0] : ""}
+                                onChange={(e) => handleDateChange(e.target.value, 'end')}
+                            />
+                            <TimeInput
+                                type="time"
+                                value={endDateTime ? endDateTime.split('T')[1].substring(0, 5): ""}
+                                onChange={(e) => handleTimeChange(e.target.value, 'end')}
+                            />
+                            <DurationSelect value={interval} onChange={handleIntervalChange}>
+                                <option value="60">1분</option>
+                                <option value="600">10분</option>
+                                <option value="1800">30분</option>
+                                <option value="3600">1시간</option>
+                            </DurationSelect>
+                            <SearchButton onClick={handleSearch}>조회</SearchButton>
+
+                        </SearchArea>
+                        <Save>
+                            <SaveDataButton onClick={handleSaveDataAsCSV}><FontAwesomeIcon icon={faFileCsv} size="2x" color={"#28a745"}/></SaveDataButton>
+                            <CaptureGraphButton onClick={handleCaptureGraph}><FontAwesomeIcon icon={faImage} size="2x" color={"#17a2b8"}/></CaptureGraphButton>
+                        </Save>
+                    </Top>
+                    <ChartArea id="graph-container">
                         <Line data={lineChartData} options={chartOptions}/>
                     </ChartArea>
                 </Contents>
@@ -168,6 +267,12 @@ const Title = styled.h1`
     margin-bottom: 20px;
 `;
 
+const Top = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin: 5px 100px 20px 20px;
+`;
+
 const Body = styled.div`
     display: flex;
     width: 100%;
@@ -181,7 +286,6 @@ const Contents = styled.div`
 const SearchArea = styled.div`
     display: flex;
     align-items: center;
-    margin-bottom: 20px;
 `;
 
 const DateInput = styled.input`
@@ -200,7 +304,6 @@ const DurationSelect = styled.select`
     cursor: pointer;
 `;
 
-
 const SearchButton = styled.button`
     padding: 5px 15px;
     background-color: #007bff;
@@ -211,6 +314,35 @@ const SearchButton = styled.button`
 
     &:hover {
         background-color: #0056b3;
+    }
+`;
+
+const Save = styled.div`
+
+`;
+
+const SaveDataButton = styled.button`
+    background-color: white;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+
+    &:hover {
+        
+    }
+`;
+
+const CaptureGraphButton = styled.button`
+    margin-left: 10px; // 버튼 사이의 간격
+    background-color: white;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+
+    &:hover {
+        
     }
 `;
 
